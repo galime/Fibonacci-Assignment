0fe9c9a191b3b5b16fefc9f901bd8a23da9c1a16
Gali Mesika <107847648+galime@users.noreply.github.com>
2022-09-13 11:01:13 +0300

    Add files via upload

--- Fibonacci Assignment/JS/Fibonacci.js
+++ Fibonacci Assignment/JS/Fibonacci.js
@@ -1,4 +1,18 @@
-var x = 'X';
-var y = 'Y';
-document.getElementById("x").innerHTML =x;
-document.getElementById("y").innerHTML =y;
+var firstNum = 0;
+var secNum = 1;
+var fiboX = 0;
+function returnFibonacci() {
+    var x = document.getElementById("num").value;
+    if (x < 2) { fiboX = x }
+    else {
+        for (let i = 1; i < x; i++) {
+            fiboX = firstNum + secNum;
+            firstNum = secNum;
+            secNum = fiboX;
+        }
+    }
+var y = fiboX;
+document.getElementById("y").innerHTML = y;
+fibox= firstNum = 0;
+secNum = 1;
+    }
